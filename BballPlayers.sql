210f7bb5b3f5869ae93add5a4c60213ae6bd9ab9
athierjung <axthierjung@gmail.com>
2015-10-16 12:32:17 -0500

    added data instances to Players table

--- BballPlayers.sql
+++ BballPlayers.sql
@@ -1,6 +1,8 @@
-CREATE OR REPLACE TRIGGER Players_trigger
-BEFORE INSERT ON Players
-FOR EACH ROW
-BEGIN
-:new.PlayerID := Players_auto_incr.nextval;
-END;
+INSERT INTO Players (PlayerName, Team, PositionName, Age) VALUES ('DerrickRose', 'Bulls', 'PointGuard', '27');
+INSERT INTO Players (PlayerName, Team, PositionName, Age) VALUES ('KevinDurant', 'Thunder', 'SmallForward', '27');
+INSERT INTO Players (PlayerName, Team, PositionName, Age) VALUES ('KyrieIrving', 'Cavs', 'PointGuard', '23');
+INSERT INTO Players (PlayerName, Team, PositionName, Age) VALUES ('KobeBryant', 'Lakers', 'ShootingGuard', '37');
+INSERT INTO Players (PlayerName, Team, PositionName, Age) VALUES ('JamesHarden', 'Rockets', 'ShootingGuard', '26');
+INSERT INTO Players (PlayerName, Team, PositionName, Age) VALUES ('BlakeGriffin', 'Clippers', 'PowerForward', '26');
+INSERT INTO Players (PlayerName, Team, PositionName, Age) VALUES ('MarcGasol', 'Grizzlies', 'Center', '30');
+INSERT INTO Players (PlayerName, Team, PositionName, Age) VALUES ('CarmeloAnthony', 'Knicks', 'SmallForward', '31');
